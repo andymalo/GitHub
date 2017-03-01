dcbb08addbf86c515fdd00e848595c47847c685f
andy <amalo.contact@gmail.com>
2017-03-02 07:22:21 +1100

    Test Commit pyCharm

--- python/udemy/readWritingFile.py
+++ python/udemy/readWritingFile.py
@@ -3,13 +3,16 @@
 #	créer un file
 #écrire à la suite séparé à un saut de ligne
 
-file = open("exemple.txt","a+")
+file = open("exemple.txt","w")
 
-lst = ["Line 1","Line 2","Line 3"]
+lst = ["Line 1","Line 2","Line 3", "Line 4","Line 5","Line 6"]
 
 for i in lst:
 	file.write(i + "\n")
 
+file.close()
+
+file = open("exemple.txt","r")
 file.seek(0)
 content = file.read()
 
